98a8f2e955cd19d9e097dce3a37970d83db98529
Pol Piella Abadia <24246926+polpielladev@users.noreply.github.com>
2023-04-04 17:35:55 +0200

    chatty-cli 1.0.2
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/chatty-cli.rb
+++ Formula/chatty-cli.rb
@@ -1,10 +1,10 @@
 class ChattyCli < Formula
   desc "A command line application to interact with ChatGPT directly from the terminal"
   homepage ""
-  url "https://github.com/polpielladev/chatty-cli/releases/download/v1.0.1/chatty-cli.tar.gz"
-  sha256 "fd153d27f8830a8d0bc5a0eaffadbdd7e5c8d7ca4943c7ad6afdec1de2f6201c"
+  url "https://github.com/polpielladev/chatty-cli/archive/v1.0.2.tar.gz"
+  sha256 "a258e0d6d96488bbcb01b51a97e2370185c0207aea7a31565f48716060eabf56"
   license ""
-  version "1.0.1"
+  version "1.0.2"
   
   def install
     bin.install "chatty"
